d2780ed9a68e28bedcd82d8fbd1b7245b51c96a0
motorny <motorny.nikita@gmail.com>
2019-02-23 00:57:08 +0300

    Updated project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='phnproxy',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/motorny/phnproxy',
+    license='MIT',
+    author='Motornyi Nikita',
+    author_email='motorny.nikita@gmail.com',
+    description='PHNode (Personal House Node) proxy package'
+)
